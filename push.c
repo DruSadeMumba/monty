da557c3c9bd89db7771ae48c02906f06dd293265
Dru Sade Mumba <drusademumba@gmail.com>
2023-08-24 10:53:34 -0700

    correction

--- push.c
+++ push.c
@@ -23,23 +23,20 @@ void push(stack_t **stack, unsigned int line)
 	int data;
 	int i = 0;
 
-	if (data_set._args != NULL && data_set._args[0] != '\0')
+	if (data_set._args)
 	{
-		if (data_set._args[0] == '-' || isdigit(data_set._args[0]))
+		if (data_set._args[0] == '-')
+			i++;
+		for (; data_set._args[i] != '\0'; i++)
 		{
-			while (data_set._args[i] != '\0')
-			{
-				if (!isdigit(data_set._args[i]))
-					push_err(stack, line);
-				i++;
-			}
-
-			data = atoi(data_set._args);
-			add_dnodeint(stack, data);
+			if (!isdigit(data_set._args[i]))
+				push_err(stack, line);
 		}
-		else
-			push_err(stack, line);
-	}
-	else
+	} else
 		push_err(stack, line);
+	data = atoi(data_set._args);
+	if (data_set._args == 0)
+		add_dnodeint(stack, data);
+	else
+		add_dnodeint_end(stack, data);
 }
